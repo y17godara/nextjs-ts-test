35e24f5f30771ce4cf0d90a245854e2bf22b0715
Yash Godara <yash17godara@gmail.com>
2023-10-25 18:56:08 +0530

    font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { fontMono, fontSans } from '@/lib/font';
 import './globals.css';
 import { siteConfig } from '@/config/site';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: {
@@ -73,7 +71,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={fontSans.variable}>{children}</body>
     </html>
   );
 }
