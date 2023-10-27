0a98c5003cf1c0ce5abfb35744561609af8b3a9e
Yash Godara <yash17godara@gmail.com>
2023-10-27 05:58:33 +0530

    Updated Packages | Next.js 14

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    serverActions: true,
-  },
   env: {
     metadataBase: 'http://localhost:3000',
   },
