fbf480eacb1142d9fdc52c275a6b3a2831e19836
Yash Godara <yash17godara@gmail.com>
2023-10-27 13:49:43 +0530

    Update page.tsx

--- app/coming-soon/page.tsx
+++ app/coming-soon/page.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import type { Metadata } from 'next';
 import bgImage from '/public/static/cs-bg-1.jpg';
 import Image from 'next/image';
+import Link from 'next/link';
+import { Icons } from '@/components/icons';
 
 export const metadata: Metadata = {
   title: 'Coming Soon',
@@ -32,13 +34,40 @@ function ComingSoon() {
             {/* top */}
             <div className='class="p-10 lg:text-start" relative text-center text-white'>
               <div className='flex h-full w-full flex-col items-center justify-center p-4 text-center'>
-                <h2>Coming Soon</h2>
+                <h2>Launching Soon...</h2>
+                <p>Welcome to my Personal Portfolio Website</p>
+                <h3>Subscribe here to get notified!</h3>
+
+                {/* Socials */}
+                <div>
+                  <h3>Follow Me:</h3>
+                  <div className='flex items-center justify-center space-x-2'>
+                    <Link
+                      href='#'
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      className='text-3xl text-white hover:text-blue-500'
+                    >
+                      <Icons.linkedin />
+                    </Link>
+                  </div>
+                </div>
               </div>
             </div>
 
             {/* bottom */}
             <div className='relative p-10 text-center text-white lg:text-start'>
               <div className='flex h-full w-full flex-col items-center justify-center p-4 text-center'>
+                <div>
+                  <Image 
+                    src='/icons/favicon.ico'
+                    alt='Logo'
+                    height={100}
+                    width={100}
+                    quality={100}
+                    priority={true}
+                  />
+                </div>
                 <div className='text-2xl font-bold'>
                   We are working on something awesome
                 </div>
