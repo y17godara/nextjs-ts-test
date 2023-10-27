77ba3f1ed4b5d832d32258cd9cf5ed6603f94098
Yash Godara <yash17godara@gmail.com>
2023-10-27 10:52:27 +0530

    responsive | grid

--- app/coming-soon/page.tsx
+++ app/coming-soon/page.tsx
@@ -15,7 +15,7 @@ function ComingSoon() {
         src={bgImage}
         alt='Coming Soon Background Image'
         layout='fill'
-        objectFit='cover'
+        // objectFit='cover'
         quality={100}
         priority={true}
         placeholder='blur'
@@ -26,9 +26,28 @@ function ComingSoon() {
       <div className='absolute left-0 top-0 flex h-full w-full bg-black opacity-50'></div>
 
       {/* Content */}
-      <div className='w-full p-2 md:w-11/12 lg:9/12 mx-auto h-full'>
-        <div className='mx-auto flex h-full w-full items-center justify-center rounded-lg bg-black/40 p-4 text-center backdrop-blur-2xl sm:w-11/12 md:w-9/12'>
-          <div className='relative text-white'>Coming Soon</div>
+      <div className='mx-auto h-full w-full p-2 sm:px-8 md:px-16 md:py-10 lg:w-11/12 lg:py-32'>
+        <div className='mx-auto flex h-full w-full items-center justify-center rounded-lg bg-black/40 p-4 text-center backdrop-blur-2xl sm:px-8 lg:w-11/12'>
+          <div className='grid items-center lg:grid-cols-2'>
+            {/* top */}
+            <div className='class="p-10 lg:text-start" relative text-center text-white'>
+              <div className='flex h-full w-full flex-col items-center justify-center p-4 text-center'>
+                <h2>Coming Soon</h2>
+              </div>
+            </div>
+
+            {/* bottom */}
+            <div className='relative p-10 text-center text-white lg:text-start'>
+              <div className='flex h-full w-full flex-col items-center justify-center p-4 text-center'>
+                <div className='text-2xl font-bold'>
+                  We are working on something awesome
+                </div>
+                <div className='mt-2 text-lg'>
+                  Please check back in sometime
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
