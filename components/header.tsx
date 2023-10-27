3874a94704546e849f30286a8db823773674edec
Yash Godara <yash17godara@gmail.com>
2023-10-27 06:07:55 +0530

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -9,21 +9,21 @@ export function Header() {
   return (
     <>
       <div className='sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
-        <div className='container flex items-center justify-between h-14'>
+        <div className='container flex h-14 items-center justify-between'>
           <div className='flex flex-row items-center gap-2'>
-            <Terminal className='w-9 h-9' />
+            <Terminal className='h-9 w-9' />
             <h2 className='h-6 font-bold'>LOGO</h2>
           </div>
           <div className='flex flex-row items-center gap-6'>
-          <Link 
-          href={"https://x.com/y17godara"} 
-          className={cn('w-0 px-0')}
-          target='_blank'
-          >
-            <Icons.twitter className='w-3 h-3 fill-current' />
-            <span className='sr-only'>Twitter</span>
-          </Link>
-          <ModeToggle />
+            <Link
+              href={'https://x.com/y17godara'}
+              className={cn('w-0 px-0')}
+              target='_blank'
+            >
+              <Icons.twitter className='h-3 w-3 fill-current' />
+              <span className='sr-only'>Twitter</span>
+            </Link>
+            <ModeToggle />
           </div>
         </div>
       </div>
