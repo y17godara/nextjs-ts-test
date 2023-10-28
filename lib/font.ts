c82c15e7016b498f053f75b8cf0efac10d8b196d
Yash Godara <yash17godara@gmail.com>
2023-10-28 22:37:49 +0530

    updates

--- lib/font.ts
+++ lib/font.ts
@@ -1,6 +1,8 @@
 import {
   JetBrains_Mono as FontMono,
   Inter as FontSans,
+  Roboto,
+  Poppins,
 } from 'next/font/google';
 
 export const fontSans = FontSans({
@@ -11,4 +13,18 @@ export const fontSans = FontSans({
 export const fontMono = FontMono({
   subsets: ['latin'],
   variable: '--font-mono',
+});
+
+export const robot = Roboto({
+  weight: ['500'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-serif',
+});
+
+export const poppins = Poppins({
+  weight: ['500'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-sans',
 });
