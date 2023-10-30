230b6931d6e9d4960078087918a81c4a7b0efb0f
Yash Godara <yash17godara@gmail.com>
2023-10-30 13:03:55 +0530

    icon

--- components/icons.tsx
+++ components/icons.tsx
@@ -248,4 +248,28 @@ export const Icons = {
       <path d='M6 12h16' />
     </svg>
   ),
+  menu: (props: IconProps) => (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className='lucide lucide-grip'
+    >
+      <circle cx='12' cy='5' r='1' />
+      <circle cx='19' cy='5' r='1' />
+      <circle cx='5' cy='5' r='1' />
+      <circle cx='12' cy='12' r='1' />
+      <circle cx='19' cy='12' r='1' />
+      <circle cx='5' cy='12' r='1' />
+      <circle cx='12' cy='19' r='1' />
+      <circle cx='19' cy='19' r='1' />
+      <circle cx='5' cy='19' r='1' />
+    </svg>
+  ),
 };
