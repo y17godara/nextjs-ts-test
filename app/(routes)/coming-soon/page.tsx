562ec94fb2a4aeb8e845f0e09e662b792523de83
Yash Godara <yash17godara@gmail.com>
2023-10-31 12:29:06 +0530

    sideNavbar | github link added

--- app/(routes)/coming-soon/page.tsx
+++ app/(routes)/coming-soon/page.tsx
@@ -18,96 +18,107 @@ function ComingSoon() {
   return (
     <Suspense fallback={<Loading />}>
       <section className={cn('relative min-h-screen w-full', nunito.variable)}>
-      <Image
-        src={bgImage}
-        alt='Coming Soon Background Image'
-        quality={100}
-        priority={true}
-        placeholder='blur'
-        className='z-[-1] bg-background/95 opacity-95 backdrop-blur supports-[backdrop-filter]:bg-background/95'
-        fill
-        sizes='100vw'
-      />
+        <Image
+          src={bgImage}
+          alt='Coming Soon Background Image'
+          quality={100}
+          priority={true}
+          placeholder='blur'
+          className='z-[-1] bg-background/95 opacity-95 backdrop-blur supports-[backdrop-filter]:bg-background/95'
+          fill
+          sizes='100vw'
+        />
 
-      {/* div: to give 50% black shade on bg image */}
-      <div className='absolute left-0 top-0 flex h-full w-full bg-black opacity-50'></div>
+        {/* div: to give 50% black shade on bg image */}
+        <div className='absolute left-0 top-0 flex h-full w-full bg-black opacity-50'></div>
 
-      {/* Content */}
-      <div className='relative flex min-h-screen items-center justify-center'>
-        <div className='relative px-4 py-2'>
-          <div className='grid max-w-[60rem] items-center rounded-lg bg-black/40 p-2 backdrop-blur-2xl lg:min-h-[40rem] lg:grid-cols-2'>
-            {/* top */}
-            <div className='relative p-4 text-center text-white lg:text-start'>
-              <div className='flex h-full w-full flex-col items-center justify-center gap-4 p-4 text-center'>
-                <div className='h-24 w-24'>
-                  <Image
-                    src='/icons/favicon.ico'
-                    alt='Logo'
-                    height={100}
-                    width={100}
-                    className='flex justify-center items-center rounded-full h-full w-full'
-                    quality={100}
-                    priority={true}
-                  />
-                </div>
-                <h2 className='text-4xl font-bold lg:mt-0'>
-                  We are working on something awesome
-                </h2>
-                <p className='text-lg font-medium text-gray-300'>
-                  Please check back in sometime
-                </p>
+        {/* Content */}
+        <div className='relative flex min-h-screen items-center justify-center'>
+          <div className='relative px-4 py-2'>
+            <div className='grid max-w-[60rem] items-center rounded-lg bg-black/40 p-2 backdrop-blur-2xl lg:min-h-[40rem] lg:grid-cols-2'>
+              {/* top */}
+              <div className='absolute top-0 right-0 justify-end flex text-end'>
+                <SliderNavbar />
               </div>
-            </div>
+              <div className='relative p-4 text-center text-white lg:text-start'>
+                <div className='flex h-full w-full flex-col items-center justify-center gap-4 p-4 text-center'>
+                  <div className='h-24 w-24'>
+                    <Image
+                      src='/icons/favicon.ico'
+                      alt='Logo'
+                      height={100}
+                      width={100}
+                      className='flex h-full w-full items-center justify-center rounded-full'
+                      quality={100}
+                      priority={true}
+                    />
+                  </div>
+                  <h2 className='text-4xl font-bold lg:mt-0'>
+                    We are working on something awesome
+                  </h2>
+                  <p className='text-lg font-medium text-gray-300'>
+                    Please check back in sometime
+                  </p>
+                </div>
+              </div>
 
-            {/* bottom */}
-            <div className='lg:text-start" relative p-4 text-center text-white'>
-              <div className='flex h-full w-full flex-col items-center justify-center p-4 text-center'>
-                <h2 className='text-4xl font-bold lg:mt-0'>
-                  Launching Soon...
-                </h2>
-                <p className='text-2xl font-medium text-gray-300'>
-                  Welcome to my Personal Portfolio Website
-                </p>
-                <h3 className='my-4 text-base font-medium text-gray-300'>
-                  Subscribe here to get notified!
-                </h3>
+              {/* bottom */}
+              <div className='lg:text-start" relative p-4 text-center text-white'>
+                <div className='flex h-full w-full flex-col items-center justify-center p-4 text-center'>
+                  <h2 className='text-4xl font-bold lg:mt-0'>
+                    Launching Soon...
+                  </h2>
+                  <p className='text-2xl font-medium text-gray-300'>
+                    Welcome to my Personal Portfolio Website
+                  </p>
+                  <h3 className='my-4 text-base font-medium text-gray-300'>
+                    Subscribe here to get notified!
+                  </h3>
 
-                {/* Email Subscribe Form */}
-                <EmailSubscribeForm />
+                  {/* Email Subscribe Form */}
+                  <EmailSubscribeForm />
 
-                {/* Socials */}
-                <div className='mt-4'>
-                  <h3 className='text-md'>Follow Me</h3>
-                  <div className='flex items-center justify-center space-x-2'>
-                    {[
-                      { name: 'LinkedIn', icon: <Icons.linkedIn />, url: '#' },
-                      { name: 'X', icon: <Icons.twitter />, url: '#' },
-                      {
-                        name: 'Instagram',
-                        icon: <Icons.instagram />,
-                        url: '#',
-                      },
-                      { name: 'Facebook', icon: <Icons.facebook />, url: '#' },
-                    ].map((item, index) => (
-                      <Link
-                        key={index}
-                        href={item.url}
-                        target='_blank'
-                        rel='noopener noreferrer'
-                        title={item.name}
-                        className='hover:text-grey-200 text-3xl text-gray-300'
-                      >
-                        {item.icon}
-                      </Link>
-                    ))}
+                  {/* Socials */}
+                  <div className='mt-4'>
+                    <h3 className='text-md'>Follow Me</h3>
+                    <div className='flex items-center justify-center space-x-2'>
+                      {[
+                        {
+                          name: 'LinkedIn',
+                          icon: <Icons.linkedIn />,
+                          url: '#',
+                        },
+                        { name: 'X', icon: <Icons.twitter />, url: '#' },
+                        {
+                          name: 'Instagram',
+                          icon: <Icons.instagram />,
+                          url: '#',
+                        },
+                        {
+                          name: 'Facebook',
+                          icon: <Icons.facebook />,
+                          url: '#',
+                        },
+                      ].map((item, index) => (
+                        <Link
+                          key={index}
+                          href={item.url}
+                          target='_blank'
+                          rel='noopener noreferrer'
+                          title={item.name}
+                          className='hover:text-grey-200 text-3xl text-gray-300'
+                        >
+                          {item.icon}
+                        </Link>
+                      ))}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
     </Suspense>
   );
 }
--- components/SliderNavbar.tsx
+++ components/SliderNavbar.tsx
@@ -13,6 +13,8 @@ import {
   SheetTitle,
   SheetTrigger,
 } from '@/components/ui/sheet';
+import { Icons } from './icons';
+import Link from 'next/link';
 
 const SHEET_SIDES = ['left'] as const;
 
@@ -20,20 +22,28 @@ type SheetSide = (typeof SHEET_SIDES)[number];
 
 function SliderNavbar() {
   return (
-    <div className='grid grid-cols-2 gap-2'>
-      {SHEET_SIDES.map((side) => (
-        <Sheet key={side}>
-          <SheetTrigger asChild>
-            <Button variant='outline'>{side}</Button>
-          </SheetTrigger>
-          <SheetContent side={side}>
-            <SheetHeader>
-              <SheetTitle>Edit profile</SheetTitle>
-              <SheetDescription>
-                Make changes to your profile here. Click save when youre done.
-              </SheetDescription>
-            </SheetHeader>
-            <div className='grid gap-4 py-4'>
+    <div className='flex flex-row items-center justify-center gap-2 p-1'>
+      <Link
+        href={'#'}
+        className='z-10 flex border border-input bg-transparent text-end shadow-sm hover:bg-accent hover:text-accent-foreground'
+      >
+        <Icons.gitHub className='h-6 w-6' />
+      </Link>
+
+      <Sheet key={'left'}>
+        <SheetTrigger asChild>
+          <Button className='z-10 flex' variant='outline'>
+            <Icons.menu className='h-6 w-6' />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side={'left'}>
+          <SheetHeader>
+            <SheetTitle>yashgodar.me</SheetTitle>
+            <SheetDescription>
+              Make changes to your profile here.
+            </SheetDescription>
+          </SheetHeader>
+          {/* <div className='grid gap-4 py-4'>
               <div className='grid grid-cols-4 items-center gap-4'>
                 <Label htmlFor='name' className='text-right'>
                   Name
@@ -46,15 +56,20 @@ function SliderNavbar() {
                 </Label>
                 <Input id='username' value='@peduarte' className='col-span-3' />
               </div>
+            </div> */}
+            <div className='grid gap-2 py-6 justify-center items-center text-center'>
+                Example content
             </div>
-            <SheetFooter>
-              <SheetClose asChild>
+          <SheetFooter>
+            {/* <SheetClose asChild>
                 <Button type='submit'>Save changes</Button>
-              </SheetClose>
-            </SheetFooter>
-          </SheetContent>
-        </Sheet>
-      ))}
+              </SheetClose> */}
+            <SheetClose asChild>
+              <Button variant='default'>Cancel</Button>
+            </SheetClose>
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
     </div>
   );
 }
